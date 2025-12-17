f7b20efbf7c9ed701683ae23314d5d307e215d70
Manasha Koirala <kmanasha22@tbc.edu.np>
2025-12-17 22:44:18 +0545

    connected database and added admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    // Helper method
+    public function getRole(): string
+    {
+        return 'admin';
+    }
+
+    public function isAdmin(): bool
+    {
+        return true;
+    }
+}
