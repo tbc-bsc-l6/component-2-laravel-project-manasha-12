526c16513307f441b00f29481efedcdfcee2c273
Manasha Koirala <kmanasha22@tbc.edu.np>
2026-01-02 19:46:17 +0545

    updated the student model to allow email verification

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class Student extends Authenticatable
+class Student extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
