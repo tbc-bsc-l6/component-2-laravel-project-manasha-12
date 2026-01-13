faf283af296cceda83b723d4659508e9ce7cc2b9
Manasha Koirala <kmanasha22@tbc.edu.np>
2026-01-13 20:29:41 +0545

    modified test cases file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use DatabaseMigrations;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Force fresh migrations for each test
+        $this->artisan('migrate:fresh');
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback');
+        });
+    }
 }
